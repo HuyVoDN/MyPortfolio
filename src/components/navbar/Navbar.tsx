0f7b9ff4e671fcc3b9a6bade398701e94142b99f
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-04-06 16:04:37 -0500

    removed error in TS

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import Button from '@mui/material/Button';
